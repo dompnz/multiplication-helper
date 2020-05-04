c19024db114a44d443bad7a8e698aaa4986cd97c
Dominic <dominic.pr93@gmail.com>
2020-05-05 00:21:35 +0200

    version1 of the app

---
+++ components/NumberGenerator.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="numbersContainer">
+    <div class="numbersInputs">
+      <div>
+        <span>Number:</span>
+        <input v-model.number="baseNumber" type="number" @change="generateNumbers()" />
+      </div>
+      <div>
+        <span>Lines:</span>
+        <input v-model.number="numberLines" type="number" @change="generateNumbers()" />
+      </div>
+    </div>
+    <ul class="numbersList">
+      <li
+        v-for="(number, index) in numbers"
+        :class="{ 'lastOfLine': (index+1) % numbersPerLine === 0 }"
+      >{{ number }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      baseNumber: 8,
+      numberLines: 10,
+      numbersPerLine: 10,
+      numbers: [],
+      items: [{ message: "Foo" }, { message: "Bar" }]
+    };
+  },
+  methods: {
+    generateNumbers() {
+      this.numbers = [];
+      let totalNumbers = this.numberLines * this.numbersPerLine;
+      for (let i = 1; i <= totalNumbers; i++) {
+        this.numbers.push(i * this.baseNumber);
+      }
+    }
+  },
+  created() {
+    this.generateNumbers();
+  }
+};
+</script>
+
+<style lang="scss">
+.numbersContainer {
+  width: 100%;
+  max-width: 60rem;
+  margin: 0 auto;
+}
+
+.numbersInputs {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  margin-bottom: 2rem;
+
+  @media (min-width: 768px) {
+    width: 50%;
+  }
+
+  & > div {
+    width: 100%;
+    display: flex;
+    align-content: space-between;
+
+    &:not(:last-child) {
+      margin-bottom: 0.5rem;
+    }
+
+    input {
+      margin-left: auto;
+      text-align: center;
+      background-color: rgba(red, 0.1);
+    }
+  }
+}
+
+ul.numbersList {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  height: 20rem;
+  overflow-y: scroll;
+  align-content: start;
+
+  @media (min-width: 768px) {
+    width: 100%;
+    height: 30rem;
+  }
+
+  li {
+    --numbers-per-line: 5;
+    width: calc(100% / var(--numbers-per-line));
+    height: 2rem;
+    //background-color: lightcoral;
+    border: 1px solid black;
+    background-color: rgba(blue, 0.1);
+    display: inline;
+    // -- center text vertically
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    // --
+
+    &.lastOfLine {
+      font-weight: bold;
+      background-color: rgba(red, 0.1);
+    }
+
+    @media (min-width: 768px) {
+      --numbers-per-line: 10;
+      height: 3rem;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <h1 class="title">Multiplication Helper</h1>
+    <div class="border-solid w-full h-auto">
+      <NumberGenerator />
+    </div>
+  </div>
+</template>
+
+<script>
+import NumberGenerator from "~/components/NumberGenerator.vue";
+
+export default {
+  components: {
+    NumberGenerator
+  }
+};
+</script>
+
+<style lang="scss">
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+body {
+  background-color: rgba(lightcoral, 0.3);
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  overflow: hidden;
+  padding: 0 1rem;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 2rem;
+  color: #35495e;
+  letter-spacing: 1px;
+  margin-bottom: 2rem;
+
+  @media (min-width: 768px) {
+    font-size: 5rem;
+  }
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
